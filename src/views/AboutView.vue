a1983503826f3f284ec4021414ad631b6396b8e7
yinsiyu <634365439@qq.com>
2024-02-26 16:07:44 +0800

    请求md文件 & 安装 Vditor 渲染md文件

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getMD } from '@/service/index'
+import { onMounted, ref } from 'vue';
+import VditorPreview from 'vditor'
+
+const md = ref('')
+const mdElement = ref<HTMLDivElement>()
+onMounted(async() => {
+  md.value = await getMD()
+  if (mdElement.value && md.value) {
+
+    console.log(mdElement.value)
+    VditorPreview.preview(mdElement.value, md.value, {mode:'dark',})
+  }
+})
+</script>
 <template>
-  <div class="text-lg">about</div>
+  <div ref="mdElement" class="vditor-reset"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
